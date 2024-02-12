abbd4366a0c760a08a191cb3c37265e133e24f3c
NikDrosakis <nikosdrosakis@gmail.com>
2024-02-13 00:01:23 +0200

    updated with env & ci/cd

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, FastAPI, WebSocket
+from fastapi import APIRouter, FastAPI
 from fastapi.responses import HTMLResponse
 router = APIRouter()
 from fastapi.encoders import jsonable_encoder
-#import uvicorn
-from models.maria import Book,insert,fetchall,update,fetch,delete
+# import uvicorn
+from models.maria import Book, insert, fetchall, update, fetch, delete
+
+
+# Load variables from .env file into environment
+
 app = FastAPI()
 html = """
 <!DOCTYPE html>
@@ -12,7 +16,7 @@ html = """
         <title>Chat</title>
     </head>
     <body>
-        <h1>APIFAST v.1.0</h1>
+        <h1>apiFAST v.1.0</h1>
     </body>
 </html>
 """
@@ -20,31 +24,39 @@ html = """
 async def root():
     return HTMLResponse(html)
 
-tables = ['cat','globs','media']
+tables = ['cat', 'globs', 'media']
 table = "globs"
+
+
 @router.get("/{table}/")
-async def getallitems():
+async def get_all_items():
     res = fetchall(table)
     return jsonable_encoder(res)
+
+
 @router.get("/{table}/{id}")
-async def getoneitem(id):
-    res = fetch(id)
+async def get_one_item(table_id):
+    res = fetch(table_id)
     return jsonable_encoder(res)
+
 
 @router.post("/{table}/")
 async def create_book_endpoint(book: Book):
- id=insert(book)
- return jsonable_encoder({"message": f"Book created {id}"})
+ table_id=insert(book)
+ return jsonable_encoder({"message": f"Book created {table_id}"})
+
 
 @router.put('/{table}/{id}')
-async def update_book(id,book: Book):
-    res = update(id,book)
+async def update_book(table_id, book: Book):
+    res = update(table_id, book)
     return jsonable_encoder({"message": f"Updated book {res}"})
+
+
 @router.delete('/{table}/{id}')
-async def delete_book(id):
-    res = delete(id)
+async def delete_book(table_id):
+    res = delete(table_id)
     return jsonable_encoder({"message": f"Deleted book {res}"})
 
-#uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
+# uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
 
 app.include_router(router)
--- models/maria.py
+++ models/maria.py
@@ -1,71 +1,75 @@
-# -*- coding: utf-8 -*-
+from dotenv import load_dotenv
 import mariadb
 import sys
-from typing import Union
+import os
 from pydantic import BaseModel
 
+load_dotenv()
 class Book(BaseModel):
- title: str
- author: str
+	title: str
+	author: str
+
 
 # Connect to MariaDB Platform
 try:
-    conn = mariadb.connect(
-        user="root",
-        password="n130177",
-        host="127.0.0.1",
-        port=9906,
-        database="nikosdrosakisgr"
+	conn = mariadb.connect(
+		user=os.getenv("MARIA_USER"),
+		password=os.getenv("MARIA_PASSWORD"),
+		host=os.getenv("MARIA_HOST"),
+		port=os.getenv("MARIA_PORT"),
+		database=os.getenv("MARIA_DB")
+	)
 
-    )
+
 except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-# Get Cursor
+	print(f"Error connecting to MariaDB Platform: {e}")
+	sys.exit(1)
 
 
-def fetch(id):
+def fetch(table_id):
 	mariac = conn.cursor(prepared=True)
-	#mariac.execute(q)
-	mariac.execute("SELECT * FROM books WHERE id=?", (id))
+	mariac.execute("SELECT * FROM books WHERE id=?", (table_id,))
 	row = mariac.fetchone()
 	mariac.close()
 	return row
 
+
 def fetchall(table):
 	mariac = conn.cursor(prepared=True)
 	mariac.execute("SELECT * FROM " + table)
-#	mariac.execute(q)
 	row = mariac.fetchall()
 	mariac.close()
 	return row
-	
-def insert(q,args):
-	mariac = conn.cursor(prepared=True)
-	mariac.execute(q, args)
-	mariac.commit()
-	if mariac.lastrowid:
-		return mariac.lastrowid
-	else:
-		return false
-	mariac.close()
-def update(q,args):
-	mariac = conn.cursor(prepared=True)
-	mariac.execute(q, args)
-	mariac.commit()
-	if mariac.lastrowid:
-		return mariac.lastrowid
-	else:
-		return false
-	mariac.close()
-def delete(q,args):
-	mariac = conn.cursor(prepared=True)
-	mariac.execute(q, args)
-	mariac.commit()
-	if mariac.lastrowid:
-		return mariac.lastrowid
-	else:
-		return false
-	mariac.close()
 
+
+def insert(q, args):
+	mariac = conn.cursor(prepared=True)
+	mariac.execute(q, args)
+	mariac.commit()
+	mariac.close()
+	if mariac.lastrowid:
+		return mariac.lastrowid
+	else:
+		return False
+
+
+def update(q, args):
+	mariac = conn.cursor(prepared=True)
+	mariac.execute(q, args)
+	mariac.commit()
+	mariac.close()
+	if mariac.lastrowid:
+		return mariac.lastrowid
+	else:
+		return False
+
+
+def delete(q, args):
+	mariac = conn.cursor(prepared=True)
+	mariac.execute(q, args)
+	mariac.commit()
+	mariac.close()
+	if mariac.lastrowid:
+		return mariac.lastrowid
+	else:
+		return False
